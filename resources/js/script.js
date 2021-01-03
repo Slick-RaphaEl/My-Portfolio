336ffd60c749c1668a405694ff7bd6ca11d82576
Raphael Otuya <57605398+Slick-RaphaEl@users.noreply.github.com>
2021-01-03 20:46:11 +0100

    Update script.js

--- resources/js/script.js
+++ resources/js/script.js
@@ -60,3 +60,28 @@ fetch(url)
 
 
   console.log("read");
+
+//get the form by its id
+const form = document.getElementById("contact-form"); 
+
+//1.
+const formEvent = form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  //2.
+  let mail = new FormData(form);
+
+  //3.
+  sendMail(mail);
+})
+
+const sendMail = (mail) => {
+  //1.
+  fetch("https://slick-raphael.github.io/My-Portfolio/send", {
+    method: "post", //2.
+    body: mail, //3.
+
+  }).then((response) => {
+    return response.json();
+  });
+};
