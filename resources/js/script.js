c3dfb127af5bfa975fbb6f8ce7bd7ca385b0e0c4
Raphael Otuya <57605398+Slick-RaphaEl@users.noreply.github.com>
2021-08-12 06:14:42 +0100

    bug fix

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,7 +1,7 @@
 
 //TTPEWRITER ANIMATION
 let i = 0;
-let txt = "from a fullstack web developer in the making.";
+let txt = "from a fullstack web developer.";
 let speed = 150;
 
 const typewriter = () => {
@@ -51,7 +51,6 @@ displaySlides(slide_index);
 
 const searchTerms = ["factory%20functions","data%20structures", "array%20functions%20javascript", "composition%20over%20inheritance", "lambda%20functions", "streams%20java", "higher%20order%functions%javascript", "functional%20programming", "c++%20lambda%20functions", "sorting%20algorithms"];
 const getSearchTerm = () => searchTerms[Math.floor(Math.random() * (searchTerms.length-1))];
-const YOUTUBE_API_KEY = "AIzaSyD7FZfDbRiGK0I8Quw40i8TcBCADjoLJXY";
 const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${getSearchTerm()}&key=${YOUTUBE_API_KEY}`;
 
 fetch(url)
@@ -59,29 +58,3 @@ fetch(url)
   .then(data => {
     document.querySelector(".youtubeVideo").src = `https://www.youtube.com/embed/${data.items[0].id.videoId}`;
 });
-
-
-  
-
-// $(document).ready(function() {
-
-//   /* Mobile navigation */
-//   $('.js--nav-icon').click(function() {
-//     var nav = $('.js--main-nav');
-//     var icon = $('.js--nav-icon i');
-    
-//     nav.slideToggle(200);
-    
-//     if (icon.hasClass('ion-navicon-round')) {
-//         icon.addClass('ion-close-round');
-//         icon.removeClass('ion-navicon-round');
-//     } else {
-//         icon.addClass('ion-navicon-round');
-//         icon.removeClass('ion-close-round');
-//     }        
-//   });
-
-// });
-
-
-console.log("read");
